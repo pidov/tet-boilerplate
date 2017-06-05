227130c376080957832839f62982af1832f7f7e3
Ivan Pidov <ivan.pidov@gmail.com>
2017-06-05 08:46:00 +0300

    Fixed generators

--- scripts/generate.js
+++ scripts/generate.js
@@ -2,5 +2,10 @@ const argv = process.argv.slice(2);
 const generatorName = argv[0]
 const generatorArguments = argv[1]
 
+if (!generatorName) {
+    console.log('Missing or invalid generator name. Valid generatorc  names are "component"')
+    return
+}
+
 const generator = require(`./generators/${generatorName}`)
 generator(generatorArguments)
