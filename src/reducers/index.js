d14554c686adf1b8762a4f02f0337c7b3787526f
Ivan Pidov <ivan.pidov@gmail.com>
2017-06-06 16:42:43 +0300

    Cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,12 @@
-import { combineReducers, routerReducer } from 'redux-seamless-immutable'
+/// ********************************************************************///
+///                      DO NOT EDIT MANUALLY!                          ///
+/// The file will be overwritten when using 'npm run generate reducer'  ///
+/// Please edit the template file (/scripts/templates/rootReducer.mst)  ///
+/// if you need to make any changes.                                    ///
+/// ********************************************************************///
+
+import {combineReducers, routerReducer} from 'redux-seamless-immutable'
+
 
 const rootReducer = combineReducers({
   routing: routerReducer
--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,14 +1,14 @@
 import { applyMiddleware, compose, createStore } from 'redux'
-import {stateTransformer} from 'redux-seamless-immutable'
+import { stateTransformer } from 'redux-seamless-immutable'
 
 import rootReducer from '~reducers'
 import initialState from './initialState'
 
-export default function configureStore (history) {
+export default function configureStore(history) {
   let middlewares = []
   let enhancers = []
 
-   if (process.env.NODE_ENV === `development`) {
+  if (process.env.NODE_ENV === `development`) {
     const { createLogger } = require(`redux-logger`)
     const logger = createLogger({ stateTransformer })
     middlewares.push(logger)
@@ -20,12 +20,11 @@ export default function configureStore (history) {
     enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__())
   }
 
-  const storeEnhancers = compose (...enhancers)
+  const storeEnhancers = compose(...enhancers)
 
   const store = createStore(rootReducer, initialState, storeEnhancers)
 
   if (module.hot) {
-    console.log('Hot reducer')
     module.hot.accept('~reducers', () => {
       const nextReducer = require('~reducers')
       store.replaceReducer(nextReducer)
